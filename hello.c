b4d32407cafc031c86b692b96487d4c06d501a30
dmaas2 <54569170+dmaas2@users.noreply.github.com>
2019-08-29 12:57:29 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: David Maas and Nicholas Halterman
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("David Maas and Nicholas Halterman\n");
+
+  return 0;
+}
